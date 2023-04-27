4ab27088dd2a315397cedf4ea65c9df27d1a99f5
audrey <mbozorgi604@gmail.com>
2023-04-27 17:56:59 +0330

    add redux and redux toolkit in repo

--- src/app/components/MainComponent/index.tsx
+++ src/app/components/MainComponent/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const MainComponent: React.FC = () => <span>hello Im main component</span>
+const MainComponent: React.FC = () => {
+    return (
+        <span>hello Im main component</span>
+    )
+}
 
 export default MainComponent
