b0f75eda915410734d642cb8f0c6e9361bbb0b26
audrey <mbozorgi604@gmail.com>
2023-04-27 12:09:02 +0330

    home component changed to function expression instead of declaration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Inter } from 'next/font/google'
 import MainComponent from './components/MainComponent'
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <MainComponent />
     </main>
   )
 }
+
+export default Home
