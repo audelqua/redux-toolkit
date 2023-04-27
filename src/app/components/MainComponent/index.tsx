f7eeb9e4e1eef9a6a62f55004545fc3587b5130c
audrey <mbozorgi604@gmail.com>
2023-04-27 12:05:01 +0330

    main component created

---
+++ src/app/components/MainComponent/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const MainComponent: React.FC = () => <span>hello Im main component</span>
+
+export default MainComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Inter } from 'next/font/google'
+import MainComponent from './components/MainComponent'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world
-      
+      <MainComponent />
     </main>
   )
 }
