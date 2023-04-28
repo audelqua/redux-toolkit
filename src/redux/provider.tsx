13d7c9df81dc105cd7346b8b774864c8b377222e
audrey <mbozorgi604@gmail.com>
2023-04-28 17:28:50 +0330

    add persistor in provider component of next js

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { store } from "./store";
+import { store, persistor } from "./store";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
+import { createWrapper } from 'next-redux-wrapper';
+
+const makeStore = () => store;
+export const wrapper = createWrapper(makeStore);
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}><PersistGate loading={null} persistor={persistor}>{children}</PersistGate></Provider>;
 }
+
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
 import counterReducer from "./features/counter/counterSlice";
 import userReducer from "./features/fetchAsync/fetchAsyncSlice";
 
+export const rootReducer = combineReducers({counterReducer, userReducer})
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    counterReducer,
-    userReducer
-  },
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
